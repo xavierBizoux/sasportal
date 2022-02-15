c484e2c84404a9db00647bdd3f4038cecab9cf16
Xavier BIZOUX <xavier.bizouxsas.com>
2022-02-15 16:10:39 +0100

    Use VA SDK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { Grid } from "@mui/material";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Grid container direction="row-reverse">
+            <Grid container item xs={3} direction="column">
+                <Grid item sx={{ minHeight:200 }}>
+                    <sas-report-object
+                        packageUri='./reports/ICU Pressure'
+                        objectName='ve25' >
+                    </sas-report-object>
+                </Grid >
+                <Grid item sx={{ minHeight: 400 }}>
+                    <sas-report-object
+                        objectName="ve33"
+                        authenticationType="credentials"
+                        url="https://live.lts.gel.race.sas.com"
+                        reportUri="/reports/reports/a92331ad-634f-4392-a57e-fcc8e3c803cf">
+                    </sas-report-object>
+                </Grid>
+            </Grid >
+        </Grid >
+    );
 }
 
-export default App;
+export default App
